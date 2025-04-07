22b2e8973a20892b3c48349678f149865076d69f
Jeongbin Kim <jake16@kaist.ac.kr>
2025-04-08 00:18:13 +0900

    update CORS setting for deployment.

--- fast/fast/__init__.py
+++ fast/fast/__init__.py
@@ -21,7 +21,12 @@ app = FastAPI()
 # Add CORS middleware with appropriate settings
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:8080"], 
+    allow_origins=[
+        "http://localhost:3000", 
+        "http://localhost:8080",
+        "https://disaster.buttercrab.net",
+        "https://api.disaster.buttercrab.net"
+    ], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
