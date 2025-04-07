7aecee12030343d0c4d9b295a53af6cdaf381160
Jeongbin Kim <jake16@kaist.ac.kr>
2025-04-08 00:22:55 +0900

    update CORS setting for deployment.

--- fast/fast/__init__.py
+++ fast/fast/__init__.py
@@ -24,8 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000", 
         "http://localhost:8080",
-        "https://disaster.buttercrab.net",
-        "https://api.disaster.buttercrab.net"
+        "https://disaster.buttercrab.net"
     ], 
     allow_credentials=True,
     allow_methods=["*"],
